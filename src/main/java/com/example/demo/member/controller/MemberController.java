8b37f43a938a08e5a4bc7255e97185aa1747490a
Hypersand <on8214@gmail.com>
2023-05-17 15:10:43 +0900

    SecurityConfig 설정 및 테스트

---
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.example.demo.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "성공!";
+    }
+
+
+}
