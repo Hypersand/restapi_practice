3f45a96ef9b4e0b1025cea8823bafa8767f32c64
Hypersand <on8214@gmail.com>
2023-05-17 17:30:21 +0900

    아이디와 비밀번호로 로그인 시도 시 토큰을 받아야 한다.

--- src/main/java/com/example/demo/member/controller/MemberController.java
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.demo.member.controller;
 
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -29,8 +30,9 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+    public Member login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
 
+        resp.addHeader("Authentication", "JWT 토큰");
         return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
     }
 
