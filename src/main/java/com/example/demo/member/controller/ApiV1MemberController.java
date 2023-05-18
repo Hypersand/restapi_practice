caaa2f5143ea945167cbc39edd14f8e53d4abb13
Hypersand <on8214@gmail.com>
2023-05-18 13:45:43 +0900

    Swagger 적용

--- src/main/java/com/example/demo/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/member/controller/ApiV1MemberController.java
@@ -3,6 +3,8 @@ package com.example.demo.member.controller;
 import com.example.demo.base.rsData.RsData;
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
+@Tag(name = "ApiV1MemberController", description = "로그인 및 로그인 된 회원의 정보")
 public class ApiV1MemberController {
 
     private final MemberService memberService;
@@ -39,6 +42,7 @@ public class ApiV1MemberController {
 
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 및 엑세스 토큰 발급")
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
 
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
@@ -53,6 +57,7 @@ public class ApiV1MemberController {
     }
 
     @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인 된 회원의 정보")
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member user1 = memberService.findByUsername(user.getUsername()).get();
 
